6c4bc59cd11e8d7924b9d8b57bf5ce68c85d34c8
Craig Cabrey <craigcabrey@gmail.com>
2015-03-10 22:18:22 -0400

    Add authentication check test

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,10 +8,8 @@
  * Controller of the tiquetmeApp
  */
 angular.module('tiquetmeApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', 'User', function($scope, User) {
+    $scope.isLoggedIn = function() {
+      return User.isAuthenticated();
+    };
+  }]);
