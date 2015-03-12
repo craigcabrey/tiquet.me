9102894b1078cbedde28b1fb64cc87d22353292c
Robert Holt <holt.r94@gmail.com>
2015-03-12 00:31:54 -0400

    (Possibly) Added logout link to dashboard

--- client/app/scripts/controllers/home.js
+++ client/app/scripts/controllers/home.js
@@ -26,6 +26,16 @@ angular.module('tiquetmeApp')
       return User.isAuthenticated();
     };
 
+    // Logout Handler
+    $scope.logout = function() {
+      User.logout({},
+        function(success) {
+          document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+          $location.path("/");
+        }
+      );
+    }
+
     // Dashboard js
     $('#side-menu').metisMenu();
     $(window).bind("load resize", function() {
