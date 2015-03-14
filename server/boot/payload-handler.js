3c23545577956db8d306c8d5ecfd0c31a3d09973
Craig Cabrey <craigcabrey@gmail.com>
2015-03-14 12:05:01 -0400

    Remove console.log

--- server/boot/payload-handler.js
+++ server/boot/payload-handler.js
@@ -1,6 +1,5 @@
 module.exports = function(app) {
   app.post('/api/payload', function(req, res) {
-    console.log('received github payload');
     console.log(JSON.stringify(req.body));
     res.json({ success: true });
   });
