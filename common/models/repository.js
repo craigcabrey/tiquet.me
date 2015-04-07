de591e082ae4b7dafa6be22ba775967a91bf7e9c
Deploy @ ProductionServer <deploy@tiquet.me>
2015-04-06 22:17:18 -0400

    Added github oauth token stuff to req

--- common/models/repository.js
+++ common/models/repository.js
@@ -1,3 +1,12 @@
-module.exports = function(repository) {
+var loopback = require('loopback');
+var app = require('../../server/server');
 
+module.exports = function(repository) {
+  repository.beforeValidate = function(next, instance) {
+    var ctx = loopback.getCurrentContext();
+    var currentUser = ctx.active.http.req.currentUser;
+    var githubOauth = currentUser.identities()[0].credentials.accessToken;
+
+    next();
+  };
 };
--- server/middleware/currentUser.js
+++ server/middleware/currentUser.js
@@ -1,19 +1,30 @@
+var loopback = require('loopback');
+
 module.exports = function() {
   return function myMiddleware(req, res, next) {
     var app = req.app;
-    if (!req.accessToken) {
-      return next();
+    if (!req.query.access_token) {
+	return next();
     }
-    app.models.User.findById(req.accessToken.userId, function(err, user) {
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        return next(new Error('No user with this access token was found.'));
-      }
-      res.locals.currentUser = user;
-      req.currentUser = user;
-      next();
+    app.models.AccessToken.findById(req.query.access_token, function(err, token) {
+	if (!err) {
+	    var userId = token.userId;
+    	    app.models.User.findOne({where: {id: userId}, include: ['identities']}, function(err, user) {
+            if (err) {
+        	return next(err);
+            }
+      	    if (!user) {
+        	return next(new Error('No user with this access token was found.'));
+            }
+            res.locals.currentUser = user;
+            req.currentUser = user;
+            var loopbackContext = loopback.getCurrentContext();
+            if (loopbackContext) {
+                loopbackContext.set('currentUser', user);
+            }
+            next();
+            });
+        }
     });
   }
 }
