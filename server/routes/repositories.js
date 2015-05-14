4b9fb329af4604974c4cb6fea1418043a631d4a3
Robert Holt <holt.r94@gmail.com>
2015-05-14 14:09:14 -0400

    This should make things slightly work

--- server/routes/repositories.js
+++ server/routes/repositories.js
@@ -2,18 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   if(!req.user) {
     return next({status: 401});
   }
-  req.db.query("SELECT * FROM repositories WHERE ownerId = ?", [req.user.id], function (err, results) {
+  req.db.query({sql: "SELECT * FROM repositories LEFT JOIN tickets ON repositories.id = tickets.repositoryId AND repositories.ownerId = ?", nestTables: true}, [req.user.id], function (err, results) {
     if (err !== null) {
       return next(err);
     }
     if (results.length === 0) {
       res.json({});
     } else {
-      res.json(results);
+      var reposList = {};
+      results.forEach(function(result) {
+        if (reposList[result.repositories.id] === undefined) {
+          reposList[result.repositories.id] = result.repositories;
+        }
+        if (reposList[result.repositories.id].tickets === undefined) {
+          console.log("new tickets array");
+          reposList[result.repositories.id].tickets = [];
+        }
+        reposList[result.repositories.id].tickets.push(result.tickets);
+      });
+      var newList = [];
+      for (var key in reposList) {
+        newList.push(reposList[key]);
+      };
+      res.json(newList);
     }
   })
 });
@@ -34,7 +49,7 @@ router.post('/', function(req, res, next) {
     repo: repoName
   }, function(err, repoRes) {
     if (err !== null) {
-      next(err);
+      return next(err);
     }
     // TODO: Check if repo exists already
     req.db.query("INSERT INTO repositories (id, name, description, created, updated, ownerName, ownerId) VALUES (?, ?, ?, ?, ?, ?, ?)",
@@ -42,7 +57,48 @@ router.post('/', function(req, res, next) {
         if (err !== null) {
           return next(err);
         }
-        res.json({success: true})
+        // Add tags
+        github.issues.getLabels({
+          user: userName,
+          repo: repoName
+        }, function(err, labelRes) {
+          if (err !== null) {
+            return next(err);
+          }
+          delete labelRes.meta;
+          var query = "INSERT INTO tags (name, color, repositoryId) VALUES ";
+          var values = [];
+          labelRes.forEach(function(label) {
+            query = query + "(?, ?, ?),";
+            values = values.concat([label.name, label.color, repoRes.id]);
+          });
+          query = query.slice(0, query.length - 1);
+          req.db.query(query, values, function(err, results) {
+            // Add tickets
+            github.issues.repoIssues({
+              user: userName,
+              repo: repoName
+            }, function(err, issueRes) {
+              if (err !== null) {
+                return next(err);
+              }
+              delete issueRes.meta;
+              var issueQuery = "INSERT INTO tickets (title, description, number, created, updated, reporterId, repositoryId) VALUES ";
+              var issueValues = [];
+              issueRes.forEach(function(issue) {
+                issueQuery = issueQuery + "(?, ?, ?, ?, ?, ?, ?),";
+                issueValues = issueValues.concat([issue.title, issue.description, issue.number, issue.created_at, issue.updated_at, issue.user.id, repoRes.id]);
+              })
+              issueQuery = issueQuery.slice(0, issueQuery.length - 1);
+              req.db.query(issueQuery, issueValues, function(err, results) {
+                if (err !== null) {
+                  return next(err);
+                }
+                res.json({success: true});
+              })
+            });
+          })
+        });
     })
   })
 });
